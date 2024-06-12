e5ee41eac97fefbd681d2c954ca1d80468f50f5c
Lorenzo <l.bartolini02@gmail.com>
2024-06-12 12:47:35 +0200

    Test with MySQL

--- src/app.py
+++ src/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword"
+)
+
 @app.route("/")
 def hello_world():
+    print(mydb)
     return "<p>Hello, World!</p>"
