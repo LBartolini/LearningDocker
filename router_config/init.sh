759104cdf640e88e2a45c26e18b6ec94d6d62932
Lorenzo <l.bartolini02@gmail.com>
2025-05-04 19:06:23 +0200

    Not working at all

---
+++ router_config/init.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+uci -q batch <<-EOF >/dev/null
+  delete network.lan
+  delete network.wan
+  delete network.wan6
+
+  # office Internet
+  set network.office=interface
+  set network.office.proto=static
+  set network.office.ifname=eth0
+  set network.office.ipaddr='172.29.0.1'
+  set network.office.dns='8.8.8.8 8.8.4.4'
+  set network.office.netmask='255.255.255.0'
+
+  # internet interface
+  set network.internet=interface
+  set network.internet.proto=static
+  set network.internet.ifname=eth1
+  set network.internet.ipaddr='172.30.0.2'
+  set network.internet.dns='8.8.8.8 8.8.4.4'
+  set network.internet.gateway='172.30.0.1'
+  set network.internet.netmask='255.255.255.0'
+
+  set network.loopback.ipaddr='127.0.0.1'
+  set network.loopback.netmask='255.0.0.0'
+
+  commit network
+
+  set uhttpd.main.rfc1918_filter=0
+  commit uhttpd
+EOF
+
+# remove old wan/lan rules (add this part if you require removing default zones and rules)
+n=$(uci show firewall | egrep "wan|lan" | wc -l)
+for i in $(seq 1 $n)
+do
+ uci delete $(uci show firewall | egrep "wan|lan" | head -1 | cut -d. -f1,2)
+done 
+
+uci commit
+
+uci -q batch <<-EOF >/dev/null
+  #set firewall.@include[0].reload='1'
+  
+  add firewall zone   
+  set firewall.@zone[-1]=zone
+  set firewall.@zone[-1].name='internet'
+  set firewall.@zone[-1].network='internet'
+  set firewall.@zone[-1].input='ACCEPT'
+  set firewall.@zone[-1].output='ACCEPT'
+  set firewall.@zone[-1].forward='ACCEPT'
+  set firewall.@zone[-1].masq='1'
+  commit firewall.@zone[-1]
+
+  add firewall zone
+  set firewall.@zone[-1]=zone
+  set firewall.@zone[-1].name='office'
+  set firewall.@zone[-1].network='office'
+  set firewall.@zone[-1].input='ACCEPT'
+  set firewall.@zone[-1].output='ACCEPT'
+  set firewall.@zone[-1].forward='ACCEPT'
+  commit firewall.@zone[-1]
+
+  # Forwarding between zones
+  add firewall forwarding
+  set firewall.@forwarding[-1].src='office'
+  set firewall.@forwarding[-1].dest='internet'
+
+  commit firewall
+EOF
+
+iptables -L > /dev/null
+
+/etc/init.d/firewall reload
+  
+exit 0
+
+
